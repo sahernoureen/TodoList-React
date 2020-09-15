4cfa4b1adc3a28b5e47d92aefab2beb40068f2af
saher noureen <sahernoureen1@gmail.com>
2020-09-15 10:56:59 -0500

    Updates

---
+++ precache-manifest.1eaf402a03a8871a34a4ed52fb6742e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec2468114005f27f708d096907732d7",
+    "url": "/TodoList-React/index.html"
+  },
+  {
+    "revision": "1ebc2a4817fa4e52a3e0",
+    "url": "/TodoList-React/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "efab6a26d17bfb2c33b6",
+    "url": "/TodoList-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1ebc2a4817fa4e52a3e0",
+    "url": "/TodoList-React/static/js/2.fbc7de05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList-React/static/js/2.fbc7de05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efab6a26d17bfb2c33b6",
+    "url": "/TodoList-React/static/js/main.0695d33b.chunk.js"
+  },
+  {
+    "revision": "f9cc69d818caa38759de",
+    "url": "/TodoList-React/static/js/runtime-main.09bb8d2f.js"
+  }
+]);
